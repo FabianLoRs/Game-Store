93b7c9b72e5bf3c19eee47a804add870ed0c1688
flopezre <sektor86@gmail.com>
2023-01-17 01:22:58 -0300

    Se elimnanor lineas comentadas innecesarias

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,7 +3,7 @@ const stockProductos = [
         id: 1, 
         nombre: "CyberPunk 2077",
         cantidad: 1, 
-        desc: "Cyberpunk 2077 es un RPG de aventura y acción de mundo abierto ambientado en el futuro sombrío de Night City, una peligrosa megalópolis obsesionada con el poder, el glamur y las incesantes modificaciones corporales", 
+        desc: "Cyberpunk 2077 es un RPG de aventura y acción de mundo abierto ambientado en el futuro sombrío de Night City.", 
         precio: 39.00, 
         url: '2077.jpg'
     },
@@ -11,7 +11,7 @@ const stockProductos = [
         id: 2, 
         nombre: "Resident Evil Village", 
         cantidad: 1, 
-        desc: "Vive el survival horror como nunca antes en la 8.ª entrega principal de la aclamada serie Resident Evil: Resident Evil Village. El terror más realista e inescapable, con gráficos hiperdetallados, intensa acción en 1.ª persona y una trama magistral.", 
+        desc: "Vive el survival horror como nunca antes en la 8.ª entrega principal de la aclamada serie Resident Evil: Resident Evil Village.", 
         precio: 32.00,
         url: 're8.jpg'
     },
@@ -19,7 +19,7 @@ const stockProductos = [
         id: 3, 
         nombre: "Resident Evil 7 Biohazard", 
         cantidad: 1, 
-        desc: "El peligro y la soledad emanan de las decrépitas paredes de una granja abandonada en el sur de los EE. UU. «7» marca un nuevo inicio para el género del survival horror, con el cambio a la aterradora perspectiva de jugador «vista aislada».", 
+        desc: "El peligro y la soledad emanan de las decrépitas paredes de una granja abandonada en el sur de los EE. UU.", 
         precio: 16.00,
         url: 're7.jpg'
     },
@@ -27,7 +27,7 @@ const stockProductos = [
         id: 4, 
         nombre: "Grand Theft Auto V",  
         cantidad: 1, 
-        desc: "Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos y el condado de Blaine con una resolución de 4K y disfrutar del juego a 60 fotogramas por segundo.", 
+        desc: "Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos.", 
         precio: 36.00,
         url: 'gta5.jpg'
     },
@@ -35,7 +35,7 @@ const stockProductos = [
         id: 5, 
         nombre: "Elden Ring", 
         cantidad: 1, 
-        desc: "EL NUEVO JUEGO DE ROL Y ACCIÓN DE AMBIENTACIÓN FANTÁSTICA. Álzate, Sinluz, y que la gracia te guíe para abrazar el poder del Círculo de Elden y encumbrarte como señor del Círculo en las Tierras Intermedias.", 
+        desc: "Álzate, Sinluz, y que la gracia te guíe para abrazar el poder del Círculo de Elden.", 
         precio: 36.00, 
         talle: "S", 
         url: 'elden-ring.jpg'
@@ -44,7 +44,7 @@ const stockProductos = [
         id: 6, 
         nombre: "EA SPORTS™ FIFA 23", 
         cantidad: 1, 
-        desc: "Vive la emoción del mayor torneo futbolístico con EA SPORTS™ FIFA 23 y la actualización de la FIFA World Cup™ masculina, ¡disponible el 9 de noviembre sin coste adicional!", 
+        desc: "Vive la emoción del mayor torneo futbolístico con EA SPORTS™ FIFA 23.", 
         precio: 43.00, 
         url: 'fifa23.jpg'
     },
@@ -52,7 +52,7 @@ const stockProductos = [
         id: 7, 
         nombre: "The Calisto Protocol", 
         cantidad: 1, 
-        desc: "Sobrevive y escapa de los horrores de Calisto. Descubre los oscuros secretos de la Luna Muerta de Júpiter.", 
+        desc: "Sobrevive y escapa de los horrores de Calisto.", 
         precio: 43.00,
         url: 'tcp.jpg'
     },
@@ -60,7 +60,7 @@ const stockProductos = [
         id: 8, 
         nombre: "Dead Space (2023)",  
         cantidad: 1, 
-        desc: "El clásico de terror de ciencia ficción, regresa con una nueva versión totalmente renovada para ofrecer una experiencia aún más inmersiva, con mejoras visuales, de sonido y de jugabilidad, además de mantenerse fiel a la emocionante visión del juego original.", 
+        desc: "El clásico de terror de ciencia ficción, regresa con una nueva versión totalmente renovada.", 
         precio: 46.00, 
         url: 'dp.jpg'
     },
@@ -77,7 +77,7 @@ const stockProductos = [
         id: 10, 
         nombre: "Forza Horizon 5",
         cantidad: 1, 
-        desc: "Explora los paisajes dinámicos en constante evolución de México gracias a la acción que vivirás al conducir los mejores coches del mundo de forma ilimitada y divertida. Arranca hacia el parque de Hot Wheels Park y prueba las pistas más extremas jamás creadas, la expansión se vende por separado.", 
+        desc: "Explora los paisajes dinámicos en constante evolución de México.", 
         precio: 43.00,
         url: 'fh5.jpg'
     },
@@ -95,7 +95,7 @@ const stockProductos = [
         id: 12, 
         nombre: "God of War (2018)", 
         cantidad: 1, 
-        desc: "Kratos ha dejado atrás su venganza contra los dioses del Olimpo y vive ahora como un hombre en los dominios de los dioses y monstruos nórdicos. En este mundo cruel e implacable debe luchar para sobrevivir… y enseñar a su hijo a hacerlo también.", 
+        desc: "Kratos ha dejado atrás su venganza contra los dioses del Olimpo y vive ahora como un hombre en los dominios de los dioses y monstruos nórdicos.", 
         precio: 32.00, 
         url: 'gow18.jpg'
     }
